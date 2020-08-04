f407ebcb0a21eb8d2bde482deedd6e1e6cd89b18
rudmer-io <rudmermartinez@gmail.com>
2020-08-04 15:03:13 -0400

    creacion del servidor express y base de datos

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express ,  { Application } from 'express';
+import morgan from 'morgan';
+
+
+export class App {
+
+    private app: Application;
+    
+    constructor (private port?: number | string) {
+        this.app = express();
+        this.settings();
+        this.middlewares();
+    }
+
+    settings() {
+        this.app.set('port', this.port || process.env.PORT || 3000)
+    }
+
+    middlewares() {
+        this.app.use(morgan('dev'))
+    }
+
+    async listen() { 
+        await this.app.listen(this.app.get('port'));
+        console.log('server on port', this.app.get('port'));
+    }
+}
+
+
---
+++ src/database.ts
@@ -0,0 +1,14 @@
+import { createPool } from 'mysql2/promise';
+
+export async function connect() {
+
+    const connection = await createPool({
+        host: 'localhost',
+        user: 'root',
+        password: 'plej7cIa.',
+        database: 'node_mysql_ts',
+        connectionLimit: 10
+    })
+    return connection;
+
+}
